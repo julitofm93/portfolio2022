a580a733d4a95f2929370934b77d7eddf55557b1
Julian <julian.fm@live.com>
2022-10-05 22:18:12 -0300

    animations added

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const boxContainer = document.querySelector(".boxContainer")
+const saluteTextContainer = document.querySelector(".saluteTextContainer")
+const saluteImgContainer = document.querySelector(".saluteImgContainer")
+const authorName = document.querySelector(".name")
+const jobTitleContainers = document.querySelectorAll(".jobTitleContainer")
+const jobTitles = document.querySelectorAll(".jobTitle")
+const projectsTitle = document.querySelector(".projectsTitle")
+const phones = document.querySelectorAll(".phone")
+const laptop = document.querySelector(".laptop")
+
 
 for(let i = 0 ; i<365; i++){
     const list = [ 
@@ -22,3 +31,19 @@ for(let i = 0 ; i<365; i++){
     el.classList = list.includes(i) ? "box active" : "box";
     boxContainer.appendChild(el)
 }
+
+window.addEventListener("scroll", ()=>{
+    let offsetY = window.scrollY
+    saluteTextContainer.style.transform= `translateY(${offsetY * 0.1}px)`
+    saluteImgContainer.style.transform= `translate(${offsetY * 0.4}px, ${offsetY * 0.7}px)`
+    authorName.style.transform= `translateX(${offsetY * 0.1}px)`
+    jobTitleContainers[0].style.backgroundPositionY = `${offsetY * 0.5}px`
+    jobTitleContainers[1].style.backgroundPositionY = `${-offsetY * 0.5}px`
+    jobTitles[0].style.transform = `translateX(calc(200vh - ${offsetY}px))`
+    jobTitles[1].style.transform = `translateX(calc(-300vh + ${offsetY}px))`
+    projectsTitle.style.transform = `translateY(calc(400vh - ${offsetY}px))`
+    phones[0].style.transform = `translateX(calc(500vh - ${offsetY}px))`
+    laptop.style.transform = `translateX(calc(600vh - ${offsetY}px))`
+    phones[1].style.transform = `translateX(calc(750vh - ${offsetY}px))`
+
+})
